1c509b66263c8c93e157348f9e42ad88030d71e0
oshinoooo <1036858975@qq.com>
2022-10-23 15:54:21 +0800

    init

---
+++ src/main/java/cn/liuyoulong/web/SpringBootTeatApplication.java
@@ -0,0 +1,11 @@
+package cn.liuyoulong.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeatApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/liuyoulong/web/SpringBootTeatApplicationTests.java
@@ -0,0 +1,12 @@
+package cn.liuyoulong.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
